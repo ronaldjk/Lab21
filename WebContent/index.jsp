ca1367f98c22b48e3b62745e8e3886d14645a729
ronaldjk <ronaldjiwonkim@gmail.com>
2018-02-19 14:14:59 -0500

    first commit maven

---
+++ WebContent/index.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Spring MVC</title>
+</head>
+<body>
+	<br>
+	<div style="text-align:center">
+		<h2>
+			Hello World!<br> <br>
+		</h2>
+		<h3>
+		<!-- The .html you have below is optional! -->
+			<a href="welcome.html">Click here to see the welcome page</a>
+			(check HomeController.java to view info for @RequestMapping("/welcome"))
+		</h3>
+	</div>
+</body>
+</html>
---
+++ src/com/gc/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.gc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller // You need to add this annotation.
+public class HomeController {
+
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>This message is coming from HomeController.java</h3></div>";
+		return new ModelAndView("welcome", "message2", message);
+	}
+}
